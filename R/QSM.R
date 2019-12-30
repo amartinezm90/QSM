2e0d85353c05a943ca00c316e25d2d047526fb4b
alex <a.martinez.m90@gmail.com>
2019-12-30 14:02:29 +0100

    MeanVector optimization Error solved (BA and BB instead of PA and PB)

--- R/QSM.R
+++ R/QSM.R
@@ -53,7 +53,7 @@ QSFunction = function(BasisA, BasisB, ndimA, ndimB, positive = TRUE, ndimLSA){
     PB = PositivePoryector(PB)
   }
 
-  stateVectorMean = rep((mean(as.matrix(PA)) + mean(as.matrix(PB)))/2,ndimLSA)
+  stateVectorMean = rep((mean(as.matrix(BA)) + mean(as.matrix(BB)))/2,ndimLSA)
   stVecOptim = optim(par = stateVectorMean, fn = stateVectorEstimationQSM, A=PA, B=PB ,
                      lower = rep(-1,ncol(PA)), upper = rep(1,ncol(PA)), method = "L-BFGS-B")$par
   stateVectorOptimTest = list(zero_optimization = (t(stVecOptim) %*% PA %*% stVecOptim) - (t(stVecOptim) %*% PB %*% stVecOptim),
