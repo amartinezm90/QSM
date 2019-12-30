36e0a52878087212583653c431ec3c9143efcde6
alex <a.martinez.m90@gmail.com>
2019-12-30 14:43:02 +0100

    more errors solved with the state vector optimization (test)

--- R/QSM.R
+++ R/QSM.R
@@ -53,11 +53,13 @@ QSFunction = function(BasisA, BasisB, ndimA, ndimB, positive = TRUE, ndimLSA){
     PB = PositivePoryector(PB)
   }
 
+
   stateVectorMean = rep((mean(as.matrix(BA)) + mean(as.matrix(BB)))/2,ndimLSA)
   stVecOptim = optim(par = stateVectorMean, fn = stateVectorEstimationQSM, A=PA, B=PB ,
                      lower = rep(-1,ncol(PA)), upper = rep(1,ncol(PA)), method = "L-BFGS-B")
   stVecOptim = stVecOptim$par
-  stateVectorOptimTest = list(zero_optimization = (t(stVecOptim) %*% PA %*% stVecOptim) - (t(stVecOptim) %*% PB %*% stVecOptim),state_vector_sum = sum(stVecOptim))
+  stateVectorOptimTest = list(zero_optimization = ((t(stVecOptim) %*% PA %*% stVecOptim) -
+                                (t(stVecOptim) %*% PB %*% stVecOptim)),state_vector_sum = sum(stVecOptim))
 
   simAB = QSM(PA,PB,stVecOptim)
   simBA = QSM(PB,PA,stVecOptim)
@@ -74,7 +76,7 @@ QSFunction = function(BasisA, BasisB, ndimA, ndimB, positive = TRUE, ndimLSA){
     theme_minimal() +
     coord_flip()
 
-  return(list(correlation_matrix = meaningfulVectors,correlation_plot = plotAB,state_vector_test = stateVectorOptimTest,
+  return(list(correlation_matrix = meaningfulVectors, correlation_plot = plotAB, state_vector_test = stateVectorOptimTest,
               AB_similarity = simAB, BA_similarity = simBA, interference_effect = interference, similarity_df = simData,
               similarity_plot = simPlot))
 }
